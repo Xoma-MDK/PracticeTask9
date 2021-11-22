2ac9a0cb92cf1311eca2daa8c46a18bced1a8d56
Daniil Molotkov <molotkov.58@ya.ru>
2021-11-22 11:25:56 +0700

    Добавьте файлы проекта.

---
+++ PracticeTask9/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace PracticeTask9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+            Console.Write("Введите размер массива А");
+            int N = int.Parse(Console.ReadLine());
+            int[] A = new int[N];
+            for (int i = 0; i < N; i++)
+            {
+                A[i] = int.Parse(Console.ReadLine());
+            }
+            Console.Write("Введите размер массива В");
+            N = int.Parse(Console.ReadLine());
+            int[] B = new int[N];
+            for (int j = 0; j < N; j++)
+            {
+                B[j] = int.Parse(Console.ReadLine());
+            }
+            int[] C;
+            int k = 0;
+            while (k < N)
+            {
+
+            }
+        }
+    }
+}
